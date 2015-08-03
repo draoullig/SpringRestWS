5fdc4b5ba0e078914d26a62017c1dcd9bf054d4f
Frederic <frederic.gillouard@gmail.com>
2015-08-03 14:24:06 +0200

    Evolution identifiant

--- src/fr/gillouard/spring/restws/controller/ClientControleur.java
+++ src/fr/gillouard/spring/restws/controller/ClientControleur.java
@@ -12,29 +12,29 @@ import fr.gillouard.spring.restws.pojo.Client;
 @RestController
 @RequestMapping("/client")
 public class ClientControleur {
-
-	/**
-	 * Liste des clients
-	 */
+	
+	 /**
+	  * Liste des clients
+	  */
 	private final List<Client> clients = new ArrayList<Client>();
 
 	/**
 	 * Constructeur.
 	 */
 	public ClientControleur() {
-		clients.add(new Client(1, "Dupont", "Jules"));
-		clients.add(new Client(2, "Martin", "Anne"));
-		clients.add(new Client(3, "Le Gall", "Bruno"));
+	 clients.add(new Client("1", "Dupont", "Jules"));
+	 clients.add(new Client("2", "Martin", "Anne"));
+	 clients.add(new Client("3", "Le Gall", "Bruno"));
 	}
-
+	
 	/**
-	 * Lister l'ensemble des clients
-	 *
-	 * @return la liste complete des clients
-	 */
+	  * Lister l'ensemble des clients
+	  *
+	  * @return la liste complete des clients
+	  */
 	@RequestMapping(method = RequestMethod.GET)
 	public List<Client> getClients() {
-		return clients;
+	 return clients;
 	}
 
 }
--- src/fr/gillouard/spring/restws/pojo/Client.java
+++ src/fr/gillouard/spring/restws/pojo/Client.java
@@ -1,81 +1,75 @@
 package fr.gillouard.spring.restws.pojo;
 
 public class Client {
+	
+	/**
+	  * Identifiant du client.
+	  */
+	private String identifiant;
 
 	/**
-	 * Identifiant du client.
-	 */
-	private int identifiant;
-
-	/**
-	 * Nom du client.
-	 */
+	  * Nom du client.
+	  */
 	private String nom;
 
 	/**
-	 * Prénom du client.
-	 */
+	  * Prénom du client.
+	  */
 	private String prenom;
 
 	/**
-	 * Constructeur.
-	 *
-	 * @param identifiant
-	 *            du client
-	 * @param nom
-	 *            du client
-	 * @param prenom
-	 *            du client
-	 */
-	public Client(final int identifiant, final String nom, final String prenom) {
-		this.identifiant = identifiant;
-		this.nom = nom;
-		this.prenom = prenom;
+	  * Constructeur.
+	  *
+	  * @param identifiant du client
+	  * @param nom du client
+	  * @param prenom du client
+	  */
+	public Client(final String identifiant, final String nom, final String prenom) {
+	 this.identifiant = identifiant;
+	 this.nom = nom;
+	 this.prenom = prenom;
 	}
 
 	/**
-	 * @return the identifiant
-	 */
-	public int getIdentifiant() {
-		return identifiant;
+	  * @return the identifiant
+	  */
+	public String getIdentifiant() {
+	 return identifiant;
 	}
 
 	/**
-	 * @param identifiant
-	 *            the identifiant to set
-	 */
-	public void setIdentifiant(final int identifiant) {
-		this.identifiant = identifiant;
+	  * @param identifiant the identifiant to set
+	  */
+	public void setIdentifiant(final String identifiant) {
+	 this.identifiant = identifiant;
 	}
 
 	/**
-	 * @return the nom
-	 */
+	  * @return the nom
+	  */
 	public String getNom() {
-		return nom;
+	 return nom;
 	}
 
 	/**
-	 * @param nom
-	 *            the nom to set
-	 */
+	  * @param nom the nom to set
+	  */
 	public void setNom(final String nom) {
-		this.nom = nom;
+	 this.nom = nom;
 	}
 
 	/**
-	 * @return the prenom
-	 */
+	  * @return the prenom
+	  */
 	public String getPrenom() {
-		return prenom;
+	 return prenom;
 	}
 
 	/**
-	 * @param prenom
-	 *            the prenom to set
-	 */
+	  * @param prenom the prenom to set
+	  */
 	public void setPrenom(final String prenom) {
-		this.prenom = prenom;
+	 this.prenom = prenom;
 	}
 
 }
